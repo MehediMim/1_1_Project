5b43a653d75f57297f2035d46bd2f9a4eca011b8
MehediMim <121716423+MehediMim@users.noreply.github.com>
2023-02-18 23:59:43 +0600

    1100

--- Test_two/iMain.cpp
+++ Test_two/iMain.cpp
@@ -3,13 +3,20 @@
 #include<stdlib.h>
 #include<string.h>
 /////////////////////////////////// COMMENTS //////////////
-// 0 for home
-// 1 for log in page
-// 2 for cna page
-// 3 for profile homepage
-// 4 for create class
-// 5 for announcement page
-// 6 for profile
+
+//@note 0 for home
+//@note 1 for log in page
+//@note 2 for create new account page
+//@note 3 for profile homepage join or create
+//@note 4 for create class
+//@note 5 for announcement page
+//@note 6 for profile
+//@note 7 for classwork
+//@note 8 for people
+//@note 9 for grades
+//@note 10 for join class
+
+int gameset = 1;
 
 /////////////////////////////////// FILE VARIABLES ////////
 FILE *username_input_file=fopen("Files/username_input_file.txt","a+");
@@ -22,6 +29,9 @@ FILE *username_checker_file=fopen("Files/username_input_file.txt","r");
 FILE *password_checker_file=fopen("Files/password_input_file.txt","r");
 
 FILE *class_code_file=fopen("Files/class_input.txt","a+");
+FILE *announcement_file=fopen("Files/announcement.txt","a+");
+FILE *joinclass_file=fopen("Files/class_input.txt","r");
+
 /////////////////////////////////// MENU VARIABLES /////////
 int showtext=0;
 int width=750,lenth=1300;
@@ -42,6 +52,8 @@ int index_password=0;
 
 char announcement_input[1000];
 int index_announcement=0;
+char joinclass_class[100];
+int index_joinclass_class=0;
 ///////////////////////////////// LOG IN VARIABLES ///////
 char username[100];
 int index_username_login=0;
@@ -51,7 +63,7 @@ char star_password[100];
 
 
 //////////////////////////////////////////////////////
-char bc[10][200]= {"Images\\bgone.bmp","Images/loginpage.bmp","Images/cnapage.bmp","Images/homepage.bmp","Images/ccpage.bmp","Images/announcement_page.bmp","Images/profile.bmp"};
+char bc[20][200]= {"Images\\bgone.bmp","Images/loginpage.bmp","Images/cnapage.bmp","Images/homepage.bmp","Images/ccpage.bmp","Images/announcement_page.bmp","Images/profile.bmp","Images/classwork.bmp","Images/people.bmp","Images/grades.bmp","Images/joinclass.bmp","Images/joinclass_home.bmp"};
 
 bool MusicOn=false;
 int gamestate=0;
@@ -60,14 +72,15 @@ int cc_substate=0;
 int login_substate=0;
 int announcement_substate=0;
 int l,i;
-int password_matched=0;
+int password_matched=1;
+int joinclass_substate=0;
 ////////////////////////////////////////////////////////
 void home_mouse(int mx,int my)
 {
     if(my>=300 && my<=550 && mx>=200 && mx<=370)
     {
         PlaySound("Sounds/enter.wav",NULL, SND_ASYNC);
-        gamestate=5;      /////////////////////////////////////// 5 for testing..set it to 1
+        gamestate=gameset;      /////////////////////////////////////// 5 for testing..set it to 1
 
     }
     if(my>=300 && my<=550 && mx>=390 && mx<=550)
@@ -99,6 +112,7 @@ void login_page()
         iFilledRectangle(585,348,309,34);
 
     }
+
     if(showtext==1)
     {
         iText(100,75,"* PASSWORD MUST BE 8 CHARACTER",GLUT_BITMAP_HELVETICA_18);
@@ -108,6 +122,14 @@ void login_page()
     iText(592,394,username,GLUT_BITMAP_HELVETICA_18);
     iSetColor(0,0,0);
     iText(592,350,star_password,GLUT_BITMAP_HELVETICA_18);
+    if(password_matched==0)
+    {
+        iSetColor(255,0,0);
+        iFilledRectangle(585,348,309,34);
+        iSetColor(0,0,0);
+        iText(590,357,"*PASSWORD DIDN'T MATCH",GLUT_BITMAP_HELVETICA_18);
+
+    }
 }
 void login_mouse(int mx,int my)
 {
@@ -270,6 +292,7 @@ void verify_files(int login_substate,int key)
         else
         {
             printf("DOESNT MATCH");
+            password_matched=0;
             strcmp(password,'\0');
             strcmp(star_password,'\0');
         }
@@ -354,7 +377,7 @@ void input_files(int cna_substate,int key)
         class_input[index_class]='\0';
         fprintf(class_input_file, "%s\n", class_input);
         fclose(class_input_file);
-        gamestate=1;
+        gamestate=0;
     }
     if(cna_substate==1)
     {
@@ -581,17 +604,21 @@ void input_in_cc( int cc_substate,int key)
 }
 void input_in_announcement(int announement_substate,int key)
 {
-    if(key==13)
-    {
-        PlaySound("Sounds/enter.wav",NULL, SND_ASYNC);
-        username_input[index_username]='\0';
-        fprintf(class_code_file, "%s\n", username_input);
-        fclose(class_code_file);
-        gamestate=5;
 
-    }
+
     if(announcement_substate==1)
     {
+        if(key==13)
+        {
+            showtext=1;
+
+            printf("%s",announcement_input);
+            PlaySound("Sounds/enter.wav",NULL, SND_ASYNC);
+            announcement_input[index_announcement]='\0';
+            fprintf(announcement_file, "%s\n",announcement_input);
+            fclose(announcement_file);
+
+        }
 
 
         if (key !='\b')
@@ -650,7 +677,7 @@ void profile_homepage_mouse(int mx,int my)
 {
     if(my>=140 && my<=300 && mx>=120 && mx<=450)
     {
-        PlaySound("Sounds/back.wav",NULL, SND_ASYNC);
+        PlaySound("Sounds/enter.wav",NULL, SND_ASYNC);
         gamestate=4;
 
     }
@@ -658,6 +685,12 @@ void profile_homepage_mouse(int mx,int my)
     {
         PlaySound("Sounds/back.wav",NULL, SND_ASYNC);
         gamestate=6;
+
+    }
+    if(my>=140 && my<=300 && mx>=450 && mx<=700)
+    {
+        PlaySound("Sounds/enter.wav",NULL, SND_ASYNC);
+        gamestate=10;
 
     }
 }
@@ -731,33 +764,47 @@ void ccmouse(int mx,int my)
 }
 void announement_mouse(int mx,int my)
 {
-    if(my>=650 && my<=750 && mx>=00 && mx<=120)
-    {
-        PlaySound("Sounds/back.wav",NULL, SND_ASYNC);
-        gamestate=3;
-
-    }
-    else if(my>=50 && my<=700 && mx>=00 && mx<=520)
+    if(my>=50 && my<=700 && mx>=00 && mx<=520)
     {
         PlaySound("Sounds/enter.wav",NULL, SND_ASYNC);
         announcement_substate=1;
-
+    }
+    if(my>=330 && my<=350 && mx>=1000 && mx<=1200)
+    {
+        PlaySound("Sounds/enter.wav",NULL, SND_ASYNC);
+        gamestate=7;
+    }
+    if(my>=290 && my<=310 && mx>=1000 && mx<=1200)
+    {
+        PlaySound("Sounds/enter.wav",NULL, SND_ASYNC);
+        gamestate=8;
+    }
+    if(my>=230 && my<=290 && mx>=1000 && mx<=1200)
+    {
+        PlaySound("Sounds/enter.wav",NULL, SND_ASYNC);
+        gamestate=9;
     }
 }
 void announcement_page()
 {
-    //strcpy(username_input,"HEllo");
     iShowBMP(0,0,bc[5]);
     iSetColor(255,255,255);
     iText(100,430,announcement_input,GLUT_BITMAP_HELVETICA_18);
     iSetColor(255,255,255);
     iText(1020,500,username_input,GLUT_BITMAP_HELVETICA_18);
+    if(showtext==1)
+    {
+        iSetColor(0,30,240);
+        iFilledRectangle(75,165,110,44);
+        iSetColor(255,255,255);
+        iText(95,180,"POSTED",GLUT_BITMAP_HELVETICA_18);
+    }
 }
 void profile()
 {
     iShowBMP(0,0,bc[6]);
     iSetColor(255,255,255);
-    iText(100,430,announcement_input,GLUT_BITMAP_HELVETICA_18);
+    iText(200,517,username,GLUT_BITMAP_HELVETICA_18);
     iSetColor(255,255,255);
     iText(1020,500,username_input,GLUT_BITMAP_HELVETICA_18);
 }
@@ -770,6 +817,99 @@ void profile_mouse(int mx,int my)
 
     }
 
+}
+void classwork()
+{
+    iShowBMP(0,0,bc[7]);
+    iSetColor(255,255,255);
+    iText(1020,500,username_input,GLUT_BITMAP_HELVETICA_18);
+}
+void people()
+{
+    iShowBMP(0,0,bc[8]);
+    iSetColor(255,255,255);
+    iText(1020,500,username_input,GLUT_BITMAP_HELVETICA_18);
+}
+void grades()
+{
+    iShowBMP(0,0,bc[9]);
+    iSetColor(255,255,255);
+    iText(1020,500,username_input,GLUT_BITMAP_HELVETICA_18);
+}
+void student_home()
+{
+    iShowBMP(0,0,bc[11]);
+    iSetColor(255,255,255);
+
+}
+void backtoannouncement_mouse(int mx,int my)
+{
+    if(my>=650 && my<=750 && mx>=00 && mx<=120)
+    {
+        PlaySound("Sounds/back.wav",NULL, SND_ASYNC);
+        gamestate=5;
+
+    }
+}
+void joinclass()
+{
+    iShowBMP(0,0,bc[10]);
+    iSetColor(255,255,255);
+    iText(1020,500,username,GLUT_BITMAP_HELVETICA_18);
+    iText(180,350,joinclass_class,GLUT_BITMAP_HELVETICA_18);
+}
+void joinclass_mouse(int mx,int my)
+{
+    if(my>=300 && my<=450 && mx>=100 && mx<=300)
+    {
+        PlaySound("Sounds/enter.wav",NULL, SND_ASYNC);
+        joinclass_substate=1;
+    }
+}
+void input_in_joinclass(int joinclass_substate,int key)
+{
+    if(joinclass_substate==1)
+    {
+        printf("%s\n",joinclass_class);
+        if(key==13)
+        {
+            PlaySound("Sounds/enter.wav",NULL, SND_ASYNC);
+            joinclass_class[index_joinclass_class]='\0';
+            char temp[100];
+            char temp2[100];
+            for(i=1; i<=10; i++)
+            {
+                fgets(temp,1000,joinclass_file);
+                sscanf(temp,"%s",&temp2);
+                if(strcmp(temp2,joinclass_class)==0)
+                {
+                    printf("class code matched\n");
+                    gamestate=11;
+                    break;
+                }
+                else printf("doesnt match\n");
+            }
+        }
+        if (key !='\b')
+        {
+            PlaySound("Sounds/enter.wav",NULL, SND_ASYNC);
+            joinclass_class[index_joinclass_class]=key;
+            joinclass_class[index_joinclass_class+1]='\0';
+            index_joinclass_class++;
+        }
+        else
+        {
+            if(index_joinclass_class>=0)
+            {
+                joinclass_class[index_joinclass_class-1]='\0';
+                index_joinclass_class--;
+            }
+            else
+            {
+                index_joinclass_class=0;
+            }
+        }
+    }
 }
 void iDraw()
 {
@@ -802,6 +942,26 @@ void iDraw()
     if(gamestate==6)
     {
         profile();
+    }
+    if(gamestate==7)
+    {
+        classwork();
+    }
+    if(gamestate==8)
+    {
+        people();
+    }
+    if(gamestate==9)
+    {
+        grades();
+    }
+    if(gamestate==10)
+    {
+        joinclass();
+    }
+    if(gamestate==11)
+    {
+        student_home();
     }
 
 }
@@ -844,6 +1004,14 @@ void iMouse(int button, int state, int mx, int my)
         {
             profile_mouse(mx,my);
         }
+        if(gamestate==7 || gamestate==8 || gamestate==9)
+        {
+            backtoannouncement_mouse(mx,my);
+        }
+        if(gamestate==10)
+        {
+            joinclass_mouse(mx,my);
+        }
 
 
 
@@ -868,6 +1036,11 @@ void iKeyboard(unsigned char key)
     {
         input_in_announcement(announcement_substate,key);
     }
+    if(gamestate==10)
+    {
+        input_in_joinclass(joinclass_substate,key);
+    }
+
 
 }
 void iSpecialKeyboard(unsigned char key)
@@ -884,6 +1057,7 @@ void iSpecialKeyboard(unsigned char key)
             cna_substate--;
         }
     }
+
 }
 int main()
 {
